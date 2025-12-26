493a53f93975ccc46683b68519602e738ffdc2dd
namubal78 <namubal78@gmail.com>
2025-12-26 15:46:39 +0900

    feat: 초기 프로젝트 설정 및 결제 DB 스키마 준비

---
+++ src/main/java/com/project/AAP_prototype/AapPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.project.AAP_prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AapPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AapPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/AAP_prototype/AapPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.AAP_prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AapPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
